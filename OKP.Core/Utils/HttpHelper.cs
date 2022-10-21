af368323a9ccc23e4e2b7308427f7435fd451a9e
sun128764 <sun128764@gmail.com>
2022-10-21 22:12:17 +0800

    feat http retry

--- OKP.Core/Utils/HttpHelper.cs
+++ OKP.Core/Utils/HttpHelper.cs
@@ -1,21 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using OKP.Core.Interface;
+using Polly;
+using Polly.Extensions.Http;
+using Polly.Retry;
 
 namespace OKP.Core.Utils
 {
     internal static class HttpHelper
     {
-        public static Task<HttpResponseMessage> PostAsyncWithRetry(this HttpClient httpClient,string? uri,HttpContent? content)
+        private static readonly AsyncRetryPolicy<HttpResponseMessage> policy = HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+        public static Task<HttpResponseMessage> PostAsyncWithRetry(this HttpClient httpClient, string? uri, HttpContent? content)
         {
-            return httpClient.PostAsync(uri, content);
+            return policy.ExecuteAsync(() => httpClient.PostAsync(uri, content));
         }
         public static Task<HttpResponseMessage> GetAsyncWithRetry(this HttpClient httpClient, string? uri)
         {
-            return httpClient.GetAsync(uri);
+            return policy.ExecuteAsync(() => httpClient.GetAsync(uri));
         }
     }
 }
