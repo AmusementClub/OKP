976bda683d8e02d7364c03f571e4ad0b5b358aa9
MIRIMIRIM <59959583+MIRIMIRIM@users.noreply.github.com>
2022-11-13 09:33:28 +0800

    add CategoryHelper

---
+++ OKP.Core/Utils/CategoryHelper.cs
@@ -0,0 +1,62 @@
+﻿using OKP.Core.Interface;
+using Serilog;
+using static OKP.Core.Utils.Constants;
+
+namespace OKP.Core.Utils
+{
+    internal class CategoryHelper
+    {
+        public static string SelectCategory(TorrentContent torrent, string site)
+        {
+            var category = "";
+            if (!SupportSiteName.Contains(site))
+            {
+                Log.Error("未受支持或输入错误的站点");
+            }
+            else
+            {
+                category = site switch
+                {
+                    "acgnx_asia" => SelectAcgnxAsia(torrent),
+                    "acgnx_global" => SelectAcgnxGlobal(torrent),
+                    _ => throw new NotImplementedException()
+                };
+            }
+            return category;
+        }
+        // Now only Anime
+        internal static string SelectAcgnxAsia(TorrentContent torrent)
+        {
+            ushort sortId;
+            if (torrent.HasSubtitle)
+            {
+                if (torrent.IsFinished)
+                {
+                    sortId = (ushort)categoryAcgnxAsia.AnimeCollection;
+                }
+                else
+                {
+                    sortId = (ushort)categoryAcgnxAsia.Anime;
+                }
+            }
+            else
+            {
+                sortId = (ushort)categoryAcgnxAsia.Raw;
+            }
+            return sortId.ToString();
+        }
+        internal static string SelectAcgnxGlobal(TorrentContent torrent)
+        {
+            ushort sortId;
+            if (torrent.HasSubtitle)
+            {
+                sortId = (ushort)categoryAcgnxGlobal.Anime;
+            }
+            else
+            {
+                sortId = (ushort)categoryAcgnxGlobal.AnimeRaw;
+            }
+            return sortId.ToString();
+        }
+    }
+}
--- OKP.Core/Utils/Constants.cs
+++ OKP.Core/Utils/Constants.cs
@@ -5,4 +5,30 @@ public static class Constants
     public const string DefaultSettingFileName = "setting.toml";
     public const string DefaultLogFileName = "log.txt";
     public const string UserPropertiesFileName = "OKP_userprop.toml";
+    public static readonly string[] SupportSiteName = { "nyaa", "dmhy", "acgrip", "acgnx_asia", "acgnx_global" };
+    
+    public enum categoryAcgnxAsia : ushort
+    {
+        Anime = 1,
+        AnimeCollection = 2,
+        Music = 6,
+        JapaneseFilm = 10,
+        Raw = 11,
+        Tokusatsu = 18,
+        Others = 19
+    }
+    public enum categoryAcgnxGlobal : ushort
+    {
+        AnimeRaw = 3,
+        Anime = 4,
+        AnimeMv = 5,
+        MusicLossless = 7,
+        MusicLossy = 8,
+        LiveActionRaw = 15,
+        LiveAction = 16,
+        LiveIdol = 17,
+        Photos = 22,
+        Graphics = 23,
+        Other = 25
+    }
 }
