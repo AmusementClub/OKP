b1251d28470dbbf6aa8d6c2bee668ce6cabd13d3
sun128764 <sun128764@gmail.com>
2023-01-04 19:00:24 +0800

    add GlobalCookieContainer

--- OKP.Core/Utils/HttpHelper.cs
+++ OKP.Core/Utils/HttpHelper.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http.Json;
+using System.Net;
 using System.Reflection.Metadata;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -16,6 +16,7 @@ namespace OKP.Core.Utils
 {
     internal static class HttpHelper
     {
+        public static CookieContainer GlobalCookieContainer = new ();
         private static readonly AsyncRetryPolicy<HttpResponseMessage> policy = HttpPolicyExtensions
                 .HandleTransientHttpError()
                 .WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
@@ -26,6 +27,23 @@ namespace OKP.Core.Utils
         public static Task<HttpResponseMessage> GetAsyncWithRetry(this HttpClient httpClient, string? uri)
         {
             return policy.ExecuteAsync(() => httpClient.GetAsync(uri));
+        }
+        public static void LoadFromJson(this CookieContainer cookieContainer, string? jsonPath)
+        {
+            if (!File.Exists(jsonPath))
+            {
+                throw new FileNotFoundException(jsonPath);
+            }
+            var cookieCollection = JsonSerializer.Deserialize<CookieCollection>(File.ReadAllText(jsonPath));
+            if (cookieCollection != null)
+            {
+                cookieContainer.Add(cookieCollection);
+            }
+        }
+        public static void SaveToJson(this CookieContainer cookieContainer, string jsonPath)
+        {
+            var jsontext = JsonSerializer.Serialize(cookieContainer.GetAllCookies());
+            File.WriteAllText(jsonPath, jsontext);
         }
         public static Task<HttpResponseMessage> PostAsJsonAsyncWithRetry<TValue>(this HttpClient httpClient, string? uri, TValue content, JsonSerializerOptions? options = null, CancellationToken cancellationToken = default)
         {
