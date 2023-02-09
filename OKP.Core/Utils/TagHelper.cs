fdd102cb305cb7b3b6a8eb7f598c08f52930e5d9
MIRIMIRIM <59959583+MIRIMIRIM@users.noreply.github.com>
2023-02-10 01:56:51 +0800

    tag support bangumi, update TagHelper

--- OKP.Core/Utils/TagHelper.cs
+++ OKP.Core/Utils/TagHelper.cs
@@ -47,6 +47,37 @@ public class Tag
         }
         return null;
     }
+
+    public List<string> FindTagAll(List<TorrentContent.ContentTypes>? types, bool isRoot = true)
+    {
+        var tags = new List<string?>();
+        // if no types specified, return the value of the current tag
+        if (types == null || types.Count == 0)
+        {
+            tags.Add(Value);
+        }
+        // If no key is specified for the current tag, search for sub-tags
+        if (Key == null || types.Contains((TorrentContent.ContentTypes)Key))
+        {
+            if (SubTags.Count > 0)
+            {
+                foreach (var child in SubTags)
+                {
+                    var tmp = child.FindTag(types, false);
+                    if (tmp != null)
+                    {
+                        tags.Add(tmp);
+                    }
+                }
+            }
+            if (Value != null)
+            {
+                tags.Add(Value);
+            }
+        }
+        tags.RemoveAll(string.IsNullOrEmpty);
+        return tags;
+    }
 }
 
 public static class TagHelper
