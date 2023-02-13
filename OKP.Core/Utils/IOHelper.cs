ce904e6034f0a43243521c57b5ae7c7ca552b97f
Rihan <sun128764@gmail.com>
2023-02-13 15:40:47 +0800

    feat add Hint func update return empty string when no reaction

--- OKP.Core/Utils/IOHelper.cs
+++ OKP.Core/Utils/IOHelper.cs
@@ -6,7 +6,17 @@ namespace OKP.Core.Utils
         public static bool NoReaction = false;
         public static string? ReadLine()
         {
-            return NoReaction ? null : Console.ReadLine();
+            return NoReaction ? "" : Console.ReadLine();
+        }
+
+        public static string basePath(string file) => Path.Combine(AppDomain.CurrentDomain.BaseDirectory, file);
+        public static void HintText(string hint)
+        {
+            var color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write(hint);
+            Console.SetCursorPosition(Console.CursorLeft - hint.Length, Console.CursorTop);
+            Console.ForegroundColor = color;
         }
     }
 }
